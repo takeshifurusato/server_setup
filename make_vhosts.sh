43d5156191d2ac314bf4bf4581e3b9aba18dbc25
takeshifurusato <takeshi.furusato@gmail.com>
2016-09-02 17:46:22 +0900

    Update make_vhosts.sh

--- make_vhosts.sh
+++ make_vhosts.sh
@@ -4,12 +4,12 @@
 # make documentroot directory
 # /var/www/www.exsample.com/htdocs
 # /var/www/www.exsample.com/logs
-mkdir -p /var/www/${VHOST_FQDN}/{htdocs,logs}
-chown -R ${OPE_USER_NAME}:apache /var/www/${VHOST_FQDN}/
+sudo mkdir -p /var/www/${VHOST_FQDN}/{htdocs,logs}
+sudo chown -R ${OPE_USER_NAME}:apache /var/www/${VHOST_FQDN}/
 
 #-------------------------------------------------------------------------------------------------------------
 # make virtual host
-cat << _EOT_ > /etc/httpd/conf.d/${VHOST_FQDN}.conf
+sudo cat << _EOT_ > /etc/httpd/conf.d/${VHOST_FQDN}.conf
 <VirtualHost *:80>
     DocumentRoot /var/www/${VHOST_FQDN}/htdocs
     ServerName ${VHOST_FQDN}
@@ -22,7 +22,7 @@ echo ${VHOST_FQDN} > /var/www/${VHOST_FQDN}/htdocs/index.html
 
 #-------------------------------------------------------------------------------------------------------------
 # make database
-mysql -uroot -p${PWD_MYSQL} << _EOT_
+sudo mysql -uroot -p${PWD_MYSQL} << _EOT_
  CREATE DATABASE ${DB_NAME};
  CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';
  GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';
@@ -31,6 +31,6 @@ _EOT_
 
 #-------------------------------------------------------------------------------------------------------------
 # service httpd restart
-service httpd restart
+sudo service httpd restart
 
 
