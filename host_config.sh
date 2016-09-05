264ee393aba9756dbabc61e238fc73a960457e7c
takeshifurusato <takeshi.furusato@gmail.com>
2016-09-05 23:02:37 +0900

    Update host_config.sh

--- host_config.sh
+++ host_config.sh
@@ -1,11 +1,6 @@
 # parameters
 export OPE_USER_NAME='opeuser'
 export OPE_USER_PASS='nr2kdURKZ'
-export PWD_MYSQL='nr2kdURKa'
-  
-# parameters
-export OPE_USER_NAME='opeuser'
-export PWD_MYSQL='nr2kdURKa'
 export HOSTFQDN='wordpress001.kensho.mobi'
 export HOSTDOMAIN='wordpress001.kensho.mobi'
 
